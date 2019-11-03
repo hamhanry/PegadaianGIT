73dea7fc01bcf33f16a5fedacdc2d0c9a8753926
Hanry Ham <hanry.h-sse2014@tggs-bangkok.org>
2019-11-03 21:59:02 +0700

    Update main.java

--- main.java
+++ main.java
@@ -6,8 +6,13 @@ public class main {
 		// TODO Auto-generated method stub
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 		System.out.println("This is version 3");
 		System.out.println("Hello World3");
+=======
+		System.out.println("This is version 1");
+		System.out.println("Hello World1");
+>>>>>>> parent of e569bc1... Version 2
 =======
 		System.out.println("This is version 1");
 		System.out.println("Hello World1");
