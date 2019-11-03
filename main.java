b6580bc649499b66aae2c47d274a5b8a84890d1d
Hanry Ham <hanry.h-sse2014@tggs-bangkok.org>
2019-11-03 20:24:22 +0700

    This is version 1
    This are the code for version 1

--- main.java
+++ main.java
@@ -1,9 +1,11 @@
+import java.io.*;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("This is version 1");
+		System.out.println("Hello World1");
 	}
 
 }
