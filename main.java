04e3a811646702ccf5028c4874561486f7f557fb
Hanry Ham <hanry.h-sse2014@tggs-bangkok.org>
2019-11-03 20:26:15 +0700

    Version 3
    This are the code for version 3

--- main.java
+++ main.java
@@ -4,8 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is version 2");
-		System.out.println("Hello World2");
+		System.out.println("This is version 3");
+		System.out.println("Hello World3");
 	}
 
 }
