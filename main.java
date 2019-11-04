d531790f2a605f0400b8e3e44437fd65a93e966b
Dewi Suryani <dewi_2a1@yahoo.com>
2019-11-04 15:09:56 +0700

    Version 5
    This is version 5

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is version 4");
+		System.out.println("This is version 5");
 		System.out.println("Hello World4");
 	}
 
