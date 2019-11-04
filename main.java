9db334830b0cbc6e7b0a2c9559f33b169dccf8f6
Dewi Suryani <dewi_2a1@yahoo.com>
2019-11-04 12:07:32 +0700

    Version 4
    Update isi

--- main.java
+++ main.java
@@ -4,28 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-		System.out.println("This is version 3");
-		System.out.println("Hello World3");
-=======
-		System.out.println("This is version 1");
-		System.out.println("Hello World1");
->>>>>>> parent of e569bc1... Version 2
-=======
-		System.out.println("This is version 1");
-		System.out.println("Hello World1");
->>>>>>> parent of e569bc1... Version 2
-=======
-		System.out.println("This is version 1");
-		System.out.println("Hello World1");
->>>>>>> parent of e569bc1... Version 2
-=======
-		System.out.println("This is version 1");
-		System.out.println("Hello World1");
->>>>>>> parent of e569bc1... Version 2
+		System.out.println("This is version 4");
+		System.out.println("Hello World4");
 	}
 
 }
