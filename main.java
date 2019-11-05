abec2903a98dbc0bda2c9aeed082c0ba0d00fafe
Hanry Ham <hanry.h-sse2014@tggs-bangkok.org>
2019-11-05 11:29:50 +0700

    Version 6
    this code for version 6

--- main.java
+++ main.java
@@ -1,11 +1,11 @@
+package pegadaianGIT;
 import java.io.*;
-
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is version 5");
-		System.out.println("Hello World4");
+		System.out.println("Version6");
+		System.out.println("Hello World6");
 	}
 
 }
